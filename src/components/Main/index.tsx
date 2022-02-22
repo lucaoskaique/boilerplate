be317a3aa65dab9f1a15baac1f7d2095559bfab2
lucaoskaique <lucas.kaique175@hotmail.com>
2022-02-22 16:26:01 -0300

    Added storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,18 @@
-//import * as S from './styles'
+import * as S from './styles'
 
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um Átomo com texto React Avançado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>Typescript, ReactJS e Styled Components</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um Desenvolvedor de frente para uma tela com código"
+    />
+  </S.Wrapper>
 )
 
 export default Main
