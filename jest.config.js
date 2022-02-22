b3fc686d92a76cb55b2d7d3d1c32feb6941f2fb7
lucaoskaique <lucas.kaique175@hotmail.com>
2022-02-22 01:49:50 -0300

    Add RTL config and first test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathPatterns: ['./node_modules', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
 }
